8f4a96cd84e1fe751a2bb271ec43a471af8f89c1
Gustavo Bizarro Lombardo <dev.biza@gmail.com>
2022-09-20 15:26:51 -0300

    2 Exercicio arrey da aula

--- src/Desafio1Array.java
+++ src/Desafio1Array.java
@@ -1,9 +1,20 @@
 public class Desafio1Array {
     public static void main(String[] args) {
-        String[] cores = {"vermelho", "preto", "Branco", "Roxo","Azul","Amarelo"};
-         System.out.println("tamanho" + cores.length);
-        System.out.println("terceiro elemento do Arrey " + cores[2]);
-        System.out.println("Priimeiro elemento Arrey é " + cores[0]);
-        System.out.println("O ultimo elemento do Arrey é " + cores[5]);
+
+        //String[] cores = {"vermelho", "preto", "Branco", "Roxo","Azul","Amarelo"};
+        //System.out.println("tamanho" + cores.length);
+        //System.out.println("terceiro elemento do Arrey " + cores[2]);
+        //System.out.println("Priimeiro elemento Arrey é " + cores[0]);
+        //System.out.println("O ultimo elemento do Arrey é " + cores[5]);
+
+        //Desafio 2
+        String[][] multicores = {
+                {"Vermelho", "Preto", "Branco"},
+                {"Roxo", "Azul"}
+        };
+        System.out.println("O primeiro elemento do primeiro ARRAY é:" + multicores[0][0]);
+        System.out.println("O primeiro elemento do segundo ARRAY é:" + multicores[1][0]);
+        System.out.println("Tamanho do ARRAY multidimensional:" + multicores.length);
+        System.out.println("tamanho primeiro ARRAY:" + multicores[0].length);
     }
 }
