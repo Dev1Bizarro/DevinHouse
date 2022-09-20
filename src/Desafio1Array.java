15cf800ef4b4e0f22e9f9e13783fce5f2b1cea30
Gustavo Bizarro Lombardo <dev.biza@gmail.com>
2022-09-20 15:00:57 -0300

    1 exercicio arrey da aula

---
+++ src/Desafio1Array.java
@@ -0,0 +1,9 @@
+public class Desafio1Array {
+    public static void main(String[] args) {
+        String[] cores = {"vermelho", "preto", "Branco", "Roxo","Azul","Amarelo"};
+         System.out.println("tamanho" + cores.length);
+        System.out.println("terceiro elemento do Arrey " + cores[2]);
+        System.out.println("Priimeiro elemento Arrey é " + cores[0]);
+        System.out.println("O ultimo elemento do Arrey é " + cores[5]);
+    }
+}
